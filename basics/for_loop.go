d324325d8a5e0ba82b60b88dd4496625e299423d
Your Name <your.email@example.com>
2026-04-07 16:10:59 +0700

    40% course for while

---
+++ basics/for_loop.go
@@ -0,0 +1,47 @@
+package basics
+
+func main() {
+
+	// Simple iteration over a range
+	// for i := 1; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// iterate over collection
+	// numbers := []int{1, 2, 3, 4, 5}
+	// for index, value := range numbers {
+	// 	fmt.Printf("Index: %v, Value: %v\n", index, value)
+	// }
+
+	// for i := 1; i <= 10; i++ {
+	// 	if i%2 == 0 {
+	// 		continue // skip the current iteration
+	// 	}
+	// 	fmt.Println("Odd number: ", i)
+	// 	if i == 5 {
+	// 		break // break out of the loop
+	// 	}
+	// }
+
+	// ASTERISK LAYOUT
+	// rows := 5
+	// // Outer loop
+	// for i := 1; i <= rows; i++ {
+	// 	// Inner loop for spaces before stars
+	// 	for j := 1; j <= rows-i; j++ {
+	// 		fmt.Print(" ")
+	// 	}
+
+	// 	// inner loop for stars
+	// 	for k := 1; k <= 2*i-1; k++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// for i := range 10 {
+	// 	i++
+	// 	fmt.Println("Number: ", i)
+	// }
+
+}

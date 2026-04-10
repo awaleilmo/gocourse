742b8dc1cdfa6a5bfe1a785fe8d76b1abb7da52a
Your Name <your.email@example.com>
2026-04-10 11:16:37 +0700

    defer 100%

---
+++ basics/defer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	prosses(73)
+}
+
+func prosses(i int) {
+	defer fmt.Println("Deffered i value:", i)
+	defer fmt.Println("First deferred statement executed")
+	defer fmt.Println("Second deferred statement executed")
+	defer fmt.Println("Third deferred statement executed")
+	i++
+	fmt.Println("Normal execution statement")
+	fmt.Println("Value of i:", i)
+}

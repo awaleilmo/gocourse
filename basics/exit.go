505abec3401bba0dbd7e0b2fdcdf7196a8a83ac2
Your Name <your.email@example.com>
2026-04-10 15:02:44 +0700

    exit 100%

---
+++ basics/exit.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	defer fmt.Println("Deferred statement")
+
+	fmt.Println("Starting the main function")
+
+	os.Exit(1)
+
+	fmt.Println("End of main function")
+}
